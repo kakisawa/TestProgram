918ccd204a08cbd88de272cf7f135beb2a80e8d8
kakisawa <uk9im3@outlook.jp>
2025-12-12 00:11:15 +0900

    up

--- Test_Dot/Collision.h
+++ Test_Dot/Collision.h
@@ -1,7 +1,16 @@
 #pragma once
+#include "DxLib.h"
 
-struct CollisionFan {
+struct CollisionFan {	// 扇形当たり判定構造体
+	VECTOR m_pos;	// 中心座標
+	VECTOR m_dir;	// 向きベクトル
+	float m_angle;	// 扇形の広がる角度(ラジアン)
+	float m_radius;	// 扇形の長さ(半径)
+};	
 
+struct ColCapsule {
+	VECTOR m_pos;		// 中心座標
+	VECTOR m_vertexPos;	// 頂点座標
 };
 
 class Collision
